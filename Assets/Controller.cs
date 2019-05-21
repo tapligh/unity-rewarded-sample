b09bdfe71f7635a620dc06964abc6de5f84e73a3
imankarimi <imankarimi.mail@gmail.com>
2019-05-21 12:48:05 +0430

    unity reward first commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controller : MonoBehaviour {
+
+    private string TOKEN = "K3R8Z6GOGK8SMJFHV7WSR9LV9FWOX4";
+    private string UNIT_CODE = "D53ADD3EDB88034464187CEAD03ADB";
+    private bool TEST_MODE = false;
+
+    private string verifyToken = "";
+
+    public Text message = null;
+
+    void Start()
+    {
+        Tapligh.Reward.Initialize(TOKEN, TEST_MODE);
+        Tapligh.Reward.OnAdListener = OnAdResult;
+        Tapligh.Reward.OnRewardReadyListener = OnRewardReady;
+        IsInitialized();
+    }
+
+    /************************************************ Main methods ************************************************/
+
+    public void LoadAd()
+    {
+        Tapligh.Reward.OnLoadReadyListener = OnAdReady;
+        Tapligh.Reward.OnLoadErrorListener = OnLoadError;
+        Tapligh.Reward.LoadAd(UNIT_CODE);
+    }
+
+    public void ShowAd()
+    {
+        Tapligh.Reward.ShowAd(UNIT_CODE);
+    }
+
+    public void VerifyToken()
+    {
+        Tapligh.Reward.OnTokenVerifyFinishedListener = OnVerifyListener;
+        Tapligh.Reward.VerifyToken(verifyToken);
+    }
+
+    public void GetTaplighVersion()
+    {
+        message.text = "Reward SDK Version : " + Tapligh.Reward.GetTaplighVersion();
+    }
+
+    /************************************************ Private methods ************************************************/
+
+    private void OnRewardReady(string reward)
+    {
+        message.text = "Reward : " + reward;
+    }
+
+    private void OnAdReady(string unit, string token)
+    {
+        Debug.Log("Unity3D Controller: On Ad Ready: VERIFY TOKEN [" + token + "] & UNIT [" + unit + "]");
+        if (message != null)
+        {
+            verifyToken = token;
+            message.text = "VERIFY TOKEN [" + token + "]";
+        }
+    }
+
+    private void OnAdResult(AdResult result, string token)
+    {
+        string msg = null;
+
+        switch (result) {
+            case AdResult.NO_INTERNET_ACSSES: msg = "NO INTERNET ACSSES"; break;
+            case AdResult.BAD_TOKEN_USED: msg = "BAD TOKEN USED"; break;
+            case AdResult.NO_AD_READY: msg = "NO AD READY"; break;
+            case AdResult.INTERNAL_ERROR: msg = "INTERNAL ERROR"; break;
+            case AdResult.TOKEN_EXPIRED: msg = "TOKEN EXPIRED"; break;
+            case AdResult.AD_CLICKED: msg = "AD CLICKED"; break;
+            case AdResult.AD_IMAGE_CLOSED: msg = "AD IMAGE CLOSED"; break;
+            case AdResult.AD_VIDEO_CLOSED_AFTER_FULL_VIEW: msg = "AD VIDEO CLOSED AFTER FULL VIEW"; break;
+            case AdResult.AD_VIDEO_CLOSED_ON_VIEW: msg = "AD VIDEO CLOSED ON VIEW"; break;
+            case AdResult.SKIP_AND_CLICKED: msg = "SKIP AND CLICKED"; break;
+            case AdResult.SKIP_AND_CLOSED: msg = "SKIP AND CLOSED"; break;
+        }
+
+        message.text = "Ad Result : " + msg;
+    }
+
+    private void OnLoadError(string unit, LoadErrorStatus error)
+    {
+        string msg = null;
+
+        switch (error) {
+            case LoadErrorStatus.NO_INTERNET_ACCSSES: msg = "NO INTERNET ACCSSES"; break;
+            case LoadErrorStatus.APP_NOT_FOUND: msg = "APP NOT FOUND"; break;
+            case LoadErrorStatus.AD_UNIT_DISABLED: msg = "AD UNIT DISABLED"; break;
+            case LoadErrorStatus.AD_UNIT_NOT_FOUND: msg = "AD UNIT NOT FOUND"; break;
+            case LoadErrorStatus.AD_UNIT_NOT_READY: msg = "AD UNIT NOT READY"; break;
+            case LoadErrorStatus.INTERNAL_ERROR: msg = "INTERNAL ERROR"; break;
+            case LoadErrorStatus.NO_AD_READY: msg = "NO AD READY"; break;
+        }
+
+        Debug.Log("Unity3D Controller: On Load Error: UNIT [" + unit + "]");
+        message.text = "On Load Error : " + msg;
+    }
+
+    private void OnVerifyListener(TokenResult tokenResult)
+    {
+        string msg = null;
+
+        switch (tokenResult) {
+            case TokenResult.INTERNAL_ERROR: msg = "ENTERNAL ERRROR"; break;
+            case TokenResult.NOT_USED: msg = "NOT USED"; break;
+            case TokenResult.SUCCESS: msg = "SUCCESS"; break;
+            case TokenResult.TOKEN_EXPIRED: msg = "TOKEN EXPIRED"; break;
+            case TokenResult.TOKEN_NOT_FOUND: msg = "TOKEN NOT FOUND"; break;
+        }
+        message.text = "Token Verify : " + msg;
+    }
+
+    private void IsInitialized()
+    {
+        message.text = "Tapligh SDK Initialized : " + Tapligh.Reward.IsInitializeDone();
+    }
+
+}
---
+++ Assets/Tapligh/Tapligh.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum AdResult
+{
+    NO_INTERNET_ACSSES,
+    BAD_TOKEN_USED,
+    NO_AD_READY,
+    INTERNAL_ERROR,
+    TOKEN_EXPIRED,
+    AD_CLICKED,
+    AD_IMAGE_CLOSED,
+    AD_VIDEO_CLOSED_AFTER_FULL_VIEW,
+    AD_VIDEO_CLOSED_ON_VIEW,
+    SKIP_AND_CLICKED,
+    SKIP_AND_CLOSED
+}
+
+public enum LoadErrorStatus
+{
+    NO_INTERNET_ACCSSES,
+    APP_NOT_FOUND,
+    AD_UNIT_DISABLED,
+    AD_UNIT_NOT_FOUND,
+    AD_UNIT_NOT_READY,
+    INTERNAL_ERROR,
+    NO_AD_READY
+}
+
+public enum TokenResult
+{
+    TOKEN_NOT_FOUND,
+    TOKEN_EXPIRED,
+    NOT_USED,
+    SUCCESS,
+    INTERNAL_ERROR
+}
+
+class Tapligh : MonoBehaviour
+{
+
+    #if !UNITY_EDITOR && UNITY_ANDROID
+        AndroidJavaClass _taplighJavaInterface;
+        AndroidJavaObject _currentActivity;
+    #endif
+
+    private static Tapligh instance;
+    private string TaplighJavaSrc = "com.tapligh.sdk.adview.TaplighUnity";
+
+    private Action<AdResult, string> _onAdListener = null;
+    public System.Action<AdResult, string> OnAdListener
+    {
+        get { return _onAdListener; }
+        set { _onAdListener = value; }
+    }
+
+    private Action<string, string> _onLoadReadyListener = null;
+    public System.Action<string, string> OnLoadReadyListener
+    {
+        get { return _onLoadReadyListener; }
+        set { _onLoadReadyListener = value; }
+    }
+
+    private Action<string, LoadErrorStatus> _onLoadErrorListener = null;
+    public System.Action<string, LoadErrorStatus> OnLoadErrorListener
+    {
+        get { return _onLoadErrorListener; }
+        set { _onLoadErrorListener = value; }
+    }
+
+    private Action<TokenResult> _onTokenVerifyFinishedListener = null;
+    public System.Action<TokenResult> OnTokenVerifyFinishedListener
+    {
+        get { return _onTokenVerifyFinishedListener; }
+        set { _onTokenVerifyFinishedListener = value; }
+    }
+
+    private Action<string> _onRewardReadyListener = null;
+    public System.Action<string> OnRewardReadyListener
+    {
+        get { return _onRewardReadyListener; }
+        set { _onRewardReadyListener = value; }
+    }
+
+    public static Tapligh Reward
+    {
+        get {
+            if (instance == null)
+            {
+                GameObject obj = new GameObject("TaplighUnityObject");
+                obj.AddComponent<Tapligh>();
+                instance = obj.GetComponent<Tapligh>();
+            }
+            return instance;
+        }
+    }
+    
+    void Awake()
+    {
+        Debug.Log("Unity3D Reward: Tapligh CREATED - CURRENT ACTIVITY IS DONE");
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    /******************************** Generate public methods for usage ********************************/
+
+    public void Initialize(string token, bool testMode)
+    {
+        Debug.Log("Unity3D Reward: Start Initializing In Java (" + TaplighJavaSrc + ")");
+
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            _taplighJavaInterface = new AndroidJavaClass(TaplighJavaSrc);
+            _taplighJavaInterface.CallStatic("initialize", token, testMode);
+            Debug.Log("Tapligh Initilizing is Done");
+        #endif
+    }
+
+    public void LoadAd(string unitCode)
+    {
+        Debug.Log("Unity3D Reward: Load Reward Ad");
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            if(_taplighJavaInterface != null){
+                _taplighJavaInterface.CallStatic("loadAd", this.gameObject.name, "onAdReady", "onLoadError", unitCode);
+            }else{
+                Debug.Log(" Tapligh Object in Unity is Null");
+            }
+        #endif
+    }
+
+    public void ShowAd(string unitCode)
+    {
+        Debug.Log("Unity3D Reward: Show Reward Ad");
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            if(_taplighJavaInterface != null) {
+                _taplighJavaInterface.CallStatic("showAd", this.gameObject.name, "onAdResult", "onRewardReady", unitCode);
+            } else { 
+                Debug.Log("Tapligh Object in Unity is Null");
+            }
+        #endif
+    }
+
+    public string GetTaplighVersion()
+    {
+        Debug.Log("Unity3D Reward: Get SDK Version");
+        string taplighVersion = "";
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            if(_taplighJavaInterface != null){
+                taplighVersion = _taplighJavaInterface.CallStatic<string>("getTaplighVersion");
+            }else{ 
+                Debug.Log("Tapligh Object in Unity is Null"); 
+            }
+        #endif
+        return taplighVersion; 
+    }
+
+    public void VerifyToken(string token)
+    {
+        Debug.Log("Unity3D Reward: Start Verify Token");
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            if(_taplighJavaInterface != null) 
+               _taplighJavaInterface.CallStatic("verifyToken", this.gameObject.name, "OnTokenVerifyJavaListener", token);
+            else 
+                Debug.Log("Tapligh Object in Unity is Null");
+        #endif
+    }
+
+    public bool IsInitializeDone()
+    {
+        Debug.Log("Unity3D Reward: Check Is Initialize Done?");
+        bool IsInitialize = false;
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            if(_taplighJavaInterface != null)
+                 IsInitialize = _taplighJavaInterface.CallStatic<bool>("isInitializeDone");
+            else
+                Debug.Log("Tapligh Object in Unity is Null");
+        #endif
+
+        return IsInitialize;
+    }
+
+    /******************************** Generate private methods for functionality ********************************/
+
+    private void onAdReady(string response)
+    {
+        Debug.Log("Unity3D Reward: On Ad Ready [" + response + "]");
+        List<string> result = GetResultArguments(response);
+        String unit = result[0];
+        String token = result[1];
+        if (_onLoadReadyListener != null) { _onLoadReadyListener(unit, token); }
+    }
+
+    private void onLoadError(string response)
+    {
+        Debug.Log("Unity3D Reward: On Load Error [" + response + "]");
+        List<string> result = GetResultArguments(response);
+        String unit = result[0];
+        String number = result[1];
+        LoadErrorStatus res_num = (LoadErrorStatus)(Int32.Parse(number));
+        if (_onLoadErrorListener != null) { _onLoadErrorListener(unit, res_num); }
+    }
+
+    private void onAdResult(string response)
+    {
+        Debug.Log("Unity3D Reward: On Ad Result [" + response + "]");
+        List<string> info = GetResultArguments(response);
+        AdResult result = (AdResult)(Int32.Parse(info[0]));
+        if (_onAdListener != null) { _onAdListener(result, info[1]); }
+    }
+
+    private void onRewardReady(string reward)
+    {
+        Debug.Log("Unity3D Reward: On Reward Ready [" + reward + "]");
+        if (_onRewardReadyListener != null) { _onRewardReadyListener(reward); }
+    }
+
+    private void OnTokenVerifyJavaListener(string response)
+    {
+        Debug.Log("Unity3D Reward: On Token Verify [" + response + "]");
+        TokenResult result = (TokenResult)(Int32.Parse(response));
+        if (_onTokenVerifyFinishedListener != null) { _onTokenVerifyFinishedListener(result); }
+    }
+
+    private List<string> GetResultArguments(string result)
+    {
+        List<string> arguments = new List<string>();
+        int deviderIndex = result.IndexOf(';');
+        arguments.Add( result.Substring( 0 , deviderIndex ) );
+        for (; deviderIndex < result.Length ; deviderIndex++) { if (result[deviderIndex] != ';') { break; } }
+        arguments.Add( result.Substring( deviderIndex)); 
+        return arguments; 
+    }
+
+}
